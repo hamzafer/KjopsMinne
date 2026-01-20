eff5013fd109e0b086ecfa5dc0059574f8a5189e
Hamza Zafar <hamzafer3@gmail.com>
2026-01-20 14:17:06 +0100

    feat(components): add meal-plan components index

---
+++ frontend/src/components/meal-plan/index.ts
@@ -0,0 +1,7 @@
+export { MealCard } from "./MealCard";
+export { MealSlot } from "./MealSlot";
+export { DayColumn } from "./DayColumn";
+export { WeeklyCalendar } from "./WeeklyCalendar";
+export { MealDetail } from "./MealDetail";
+export { AddMealForm } from "./AddMealForm";
+export { LeftoverCard } from "./LeftoverCard";
